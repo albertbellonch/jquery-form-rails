147adf6f072e72159cb0fd870f5855e6189a7f04
Michael Sievers <michael_sievers@web.de>
2012-07-05 13:55:12 +0200

    BUGFIX: Added misssing colon

--- lib/jquery-form-rails.rb
+++ lib/jquery-form-rails.rb
@@ -3,7 +3,7 @@ require "jquery-form-rails/version"
 module Jquery
   module Form
     module Rails
-      class Engine < ::Rails:Engine
+      class Engine < ::Rails::Engine
       end
     end
   end
